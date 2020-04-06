99f975a326c3765ae0f7b89aeeacee7f489c22aa
Xu Shaohua <shaohua@biofan.org>
2020-04-06 12:14:17 +0800

    examples: Add async pub

---
+++ examples/async_pub.rs
@@ -0,0 +1,20 @@
+// Copyright (c) 2020 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+use ruo::{
+    AsyncClient,
+    ConnectOptions,
+    QoSLevel,
+};
+
+#[tokio::main]
+async fn main() {
+    std::env::set_var("RUST_LOG", "info");
+    env_logger::init();
+
+    let addr = "127.0.0.1:1883";
+    let options = ConnectOptions::new(addr).unwrap();
+    let mut client = AsyncClient::connect(options).await.unwrap();
+    client.publish("hello", QoSLevel::QoS0, b"Hello, world").await;
+}
