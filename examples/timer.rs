f4f8d2f8af85f9c98b1b05bc9721094f3d88a82f
Xu Shaohua <shaohua@biofan.org>
2020-04-07 23:50:32 +0800

    example: Add timer

---
+++ examples/timer.rs
@@ -0,0 +1,15 @@
+// Copyright (c) 2020 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+use std::time::Duration;
+use tokio::time::interval;
+
+#[tokio::main]
+async fn main() {
+    let mut timer = interval(Duration::from_millis(500));
+    loop {
+        println!("tick()");
+        timer.tick().await;
+    }
+}
