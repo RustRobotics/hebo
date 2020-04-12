f60056813b0567e6003b36f32222d83bd0f51a45
Xu Shaohua <shaohua@biofan.org>
2020-04-12 22:31:44 +0800

    Add main loop

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,20 @@
+use std::io;
+use tokio::net::{TcpListener, TcpStream};
 
-fn main() {
-    println!("Hello, world!");
+async fn process_socket(socket: TcpStream) {
+    log::info!("process socket!");
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    std::env::set_var("RUST_LOG", "log");
+    env_logger::init();
+
+    let mut listener = TcpListener::bind("127.0.0.1:1883").await?;
+    loop {
+        match listener.accept().await {
+            Ok((socket, _)) => process_socket(socket).await,
+            Err(err) => log::error!("Failed to accept incoming connection: {:?}", err),
+        }
+    }
 }
