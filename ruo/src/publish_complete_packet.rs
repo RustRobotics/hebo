c025663b2a624f9a61968b5612445500aab813d5
Xu Shaohua <shaohua@biofan.org>
2020-04-13 18:54:17 +0800

    Impl ToNetPacket for PublishReceivedPacket

--- ruo/src/publish_complete_packet.rs
+++ ruo/src/publish_complete_packet.rs
@@ -4,7 +4,8 @@
 
 use crate::base::*;
 use crate::error::Error;
-use byteorder::{BigEndian, ByteOrder};
+use byteorder::{BigEndian, ByteOrder, WriteBytesExt};
+use std::io;
 
 #[derive(Clone, Debug, Default, Eq, Hash, PartialEq)]
 pub struct PublishCompletePacket {
@@ -29,5 +30,23 @@ impl FromNetPacket for PublishCompletePacket {
         *offset += 2;
 
         Ok(PublishCompletePacket { packet_id })
+    }
+}
+
+impl ToNetPacket for PublishCompletePacket {
+    fn to_net(&self, buf: &mut Vec<u8>) -> io::Result<usize> {
+        let old_len = buf.len();
+
+        let fixed_header = FixedHeader {
+            packet_type: PacketType::PublishComplete,
+            packet_flags: PacketFlags::PublishComplete,
+        };
+        fixed_header.to_net(buf)?;
+
+        let remaining_len = 2;
+        buf.push(remaining_len);
+        buf.write_u16::<BigEndian>(self.packet_id)?;
+
+        Ok(buf.len() - old_len)
     }
 }
