96a9f51a84255dd1dd705e500e628e05467e461b
Xu Shaohua <xushaohua2016@outlook.com>
2020-04-25 22:01:23 +0800

    codec: Convert Utf8StringError to Error type

--- codec/src/error.rs
+++ codec/src/error.rs
@@ -8,9 +8,26 @@ pub enum Error {
     InvalidFixedHeader,
     PacketEmpty,
     InvalidQoS,
+
+    /// Protocol level is not in `3.1`, `3.1.1` or `5.0`.
     InvalidProtocolLevel,
+
+    /// ClientId is empty or its length exceeds 23.
+    /// Or contains invalid characters.
     InvalidClientId,
+
+    /// Length of data exceeds its limitation
+    TooManyData,
+
+    /// Invalid UTF-8 string.
+    InvalidString,
 
     /// Length of buffer - offset < remaining length.
     InvalidRemainingLength,
+}
+
+impl From<std::string::FromUtf8Error> for Error {
+    fn from(_e: std::string::FromUtf8Error) -> Error {
+        Error::InvalidString
+    }
 }
