d3348f8e0fb6a1d8d3981c02a4392ed26dccf491
Xu Shaohua <xushaohua2016@outlook.com>
2020-04-25 22:16:21 +0800

    ruo: Fix parameter error

--- ruo/src/async_client.rs
+++ ruo/src/async_client.rs
@@ -60,8 +60,7 @@ impl AsyncClient {
             publishing_qos2_packets: HashMap::new(),
         };
 
-        let mut conn_packet = ConnectPacket::new();
-        conn_packet.set_client_id(client.connect_options.client_id());
+        let conn_packet = ConnectPacket::new(client.connect_options.client_id());
         client.send(conn_packet).await;
         log::info!("send conn packet");
 
