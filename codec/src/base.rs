ba82baa9b9d59d37bb63a461f4ac4ead38c97e55
Xu Shaohua <xushaohua2016@outlook.com>
2020-04-25 22:01:39 +0800

    codec: Add validation functions

--- codec/src/base.rs
+++ codec/src/base.rs
@@ -418,3 +418,35 @@ mod tests {
         assert_eq!(ret.unwrap().0, 268_435_455);
     }
 }
+
+/// Check string characters and length.
+pub fn validate_utf8_string(s: &str) -> Result<(), Error> {
+    if s.len() > u16::MAX as usize {
+        return Err(Error::TooManyData);
+    }
+    for c in s.chars() {
+        // Ignore control characters
+        // No need to check chars between 0xd800 and 0xfffd as they are invalid coded point and not allowed.
+        if (c >= '\u{0000}' && c <= '\u{001f}') || (c >= '\u{007f}' && c <= '\u{009f}') {
+            return Err(Error::InvalidString);
+        }
+    }
+    // Empty string is valid.
+    Ok(())
+}
+
+/// Convert range of bytes to valid UTF-8 string.
+pub fn to_utf8_string(buf: &[u8], start: usize, end: usize) -> Result<String, Error> {
+    let s = String::from_utf8((&buf[start..end]).to_vec())?;
+    validate_utf8_string(&s)?;
+    Ok(s)
+}
+
+/// Check data length exceeds 64k or not.
+pub fn validate_two_bytes_data(data: &[u8]) -> Result<(), Error> {
+    if data.len() > u16::MAX as usize {
+        Err(Error::TooManyData)
+    } else {
+        Ok(())
+    }
+}
