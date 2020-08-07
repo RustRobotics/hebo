bbea54df78c013ebed5c49c5e3b5ff015f6de9ac
Xu Shaohua <shaohua@biofan.org>
2020-08-07 08:08:27 +0800

    Update dependencies

--- ruo/src/lib.rs
+++ ruo/src/lib.rs
@@ -2,6 +2,8 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
+#![feature(type_alias_impl_trait)]
+
 pub mod async_client;
 pub mod connect_options;
 pub mod stream;
