161d26170c8534b30a93fcd636f9fcdca3ebcdc5
Xu Shaohua <shaohua@biofan.org>
2020-11-19 13:53:38 +0800

    codec: Add is_valid_topic_name()

--- codec/src/base.rs
+++ codec/src/base.rs
@@ -361,6 +361,85 @@ impl TryFrom<u8> for QoS {
     }
 }
 
+/// Check string characters and length.
+pub fn validate_utf8_string(s: &str) -> Result<(), Error> {
+    if s.len() > u16::MAX as usize {
+        return Err(Error::TooManyData);
+    }
+    for c in s.chars() {
+        // Ignore control characters
+        // No need to check chars between 0xd800 and 0xfffd as they are invalid coded point and not allowed.
+        if (c >= '\u{0000}' && c <= '\u{001f}') || (c >= '\u{007f}' && c <= '\u{009f}') {
+            return Err(Error::InvalidString);
+        }
+    }
+    // Empty string is valid.
+    Ok(())
+}
+
+/// Convert range of bytes to valid UTF-8 string.
+pub fn to_utf8_string(buf: &[u8], start: usize, end: usize) -> Result<String, Error> {
+    if end > buf.len() {
+        return Err(Error::InvalidString);
+    }
+    let s = String::from_utf8((&buf[start..end]).to_vec())?;
+    validate_utf8_string(&s)?;
+    Ok(s)
+}
+
+/// Check data length exceeds 64k or not.
+pub fn validate_two_bytes_data(data: &[u8]) -> Result<(), Error> {
+    if data.len() > u16::MAX as usize {
+        Err(Error::TooManyData)
+    } else {
+        Ok(())
+    }
+}
+
+/// Check whether topic name contains wildchard characters.
+/// ```
+/// use codec::base::is_valid_topic_name;
+/// let name = "sport/tennis/player/#";
+/// assert_eq!(is_valid_topic_name(name), false);
+/// ```
+pub fn is_valid_topic_name(topic: &str) -> bool {
+    topic.bytes().filter(|c| c == &b'#' || c == &b'#').next() == None
+}
+
+/// Validate topic filter.
+/// ```
+/// use codec::base::is_valid_topic_filter;
+/// let name = "sport/tennis/player/#";
+/// assert_eq!(is_valid_topic_filter(name), true);
+///
+/// let name = "sport/tennis/player#";
+/// assert_eq!(is_valid_topic_filter(name), false);
+///
+/// let name = "#";
+/// assert_eq!(is_valid_topic_filter(name), true);
+///
+/// let name = "sport/#/player/ranking";
+/// assert_eq!(is_valid_topic_filter(name), false);
+/// ```
+pub fn is_valid_topic_filter(topic: &str) -> bool {
+    if topic == "#" {
+        return true;
+    }
+    let bytes = topic.as_bytes();
+    for (index, b) in bytes.iter().enumerate() {
+        if b == &b'#' {
+            if bytes[index - 1] != b'/' {
+                return false;
+            }
+            if index != bytes.len() - 1 {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
 #[cfg(test)]
 mod tests {
     use super::RemainingLength;
@@ -417,40 +496,5 @@ mod tests {
         let mut offset = 0;
         let ret = RemainingLength::from_net(&buf, &mut offset);
         assert_eq!(ret.unwrap().0, 268_435_455);
-    }
-}
-
-/// Check string characters and length.
-pub fn validate_utf8_string(s: &str) -> Result<(), Error> {
-    if s.len() > u16::MAX as usize {
-        return Err(Error::TooManyData);
-    }
-    for c in s.chars() {
-        // Ignore control characters
-        // No need to check chars between 0xd800 and 0xfffd as they are invalid coded point and not allowed.
-        if (c >= '\u{0000}' && c <= '\u{001f}') || (c >= '\u{007f}' && c <= '\u{009f}') {
-            return Err(Error::InvalidString);
-        }
-    }
-    // Empty string is valid.
-    Ok(())
-}
-
-/// Convert range of bytes to valid UTF-8 string.
-pub fn to_utf8_string(buf: &[u8], start: usize, end: usize) -> Result<String, Error> {
-    if end > buf.len() {
-        return Err(Error::InvalidString);
-    }
-    let s = String::from_utf8((&buf[start..end]).to_vec())?;
-    validate_utf8_string(&s)?;
-    Ok(s)
-}
-
-/// Check data length exceeds 64k or not.
-pub fn validate_two_bytes_data(data: &[u8]) -> Result<(), Error> {
-    if data.len() > u16::MAX as usize {
-        Err(Error::TooManyData)
-    } else {
-        Ok(())
     }
 }
