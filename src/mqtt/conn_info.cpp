e074db79e78028eaf4e2fc7a0762826298c604b7
Xu Shaohua <shaohua@biofan.org>
2021-02-23 15:07:46 +0800

    mqtt: Add ConnInfo

---
+++ src/mqtt/conn_info.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+#include "mqtt/conn_info.h"
+
+namespace hebo {
+
+QDebug operator<<(QDebug stream, const ConnInfo& info) {
+  stream << "ConnInfo {"
+         << "\n  name:" << info.name
+         << "\n  clientId:" << info.client_id
+         << "\n  host:" << info.host
+         << "\n  port:" << info.port
+         << "\n  username:" << info.username
+         << "\n  password:" << info.password
+         << "\n  tls:" << info.with_tls
+         << "\n  cleanSession:" << info.clean_session
+         << "}";
+  return stream;
+}
+
+}  // namespace hebo
---
+++ src/mqtt/conn_info.h
@@ -0,0 +1,39 @@
+// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+#ifndef HEBOUI_SRC_MQTT_CONN_INFO_H_
+#define HEBOUI_SRC_MQTT_CONN_INFO_H_
+
+#include <QDebug>
+#include <QMetaType>
+#include <QString>
+
+namespace hebo {
+
+enum class QoS : uint8_t {
+  kAtMostOnce,
+  kAtLeaseOnce,
+  kExactOnce,
+};
+
+struct ConnInfo {
+  QString name{};
+  QString client_id{};
+  QString protocol{};
+  QString host{};
+  uint16_t port{};
+  QoS qos{QoS::kAtMostOnce};
+  QString username{};
+  QString password{};
+  bool with_tls{false};
+  bool clean_session;
+};
+
+QDebug operator<<(QDebug stream, const ConnInfo& info);
+
+}  // namespace hebo
+
+Q_DECLARE_METATYPE(hebo::ConnInfo);
+
+#endif  // HEBOUI_SRC_MQTT_CONN_INFO_H_
