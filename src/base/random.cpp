0d786ee2f27d12957a13cd2315ee2f3759844cf1
Xu Shaohua <shaohua@biofan.org>
2021-03-05 16:10:27 +0800

    base: Add random client id generator

---
+++ src/base/random.cpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+#include "base/random.h"
+
+#include <QDateTime>
+#include <QDebug>
+#include <QRandomGenerator>
+
+namespace hebo {
+
+QString randomClientId() {
+  auto* rng = QRandomGenerator::global();
+  const qint64 time =  QDateTime::currentMSecsSinceEpoch();
+  const quint64 num = rng->generate64() + time;
+  return QString::number(num, 16).right(8);
+}
+
+}  // namespace hebo
---
+++ src/base/random.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+#ifndef HEBOUI_SRC_BASE_RANDOM_H_
+#define HEBOUI_SRC_BASE_RANDOM_H_
+
+#include <QString>
+
+namespace hebo {
+
+QString randomClientId();
+
+}  // namespace hebo
+
+#endif  // HEBOUI_SRC_BASE_RANDOM_H_
