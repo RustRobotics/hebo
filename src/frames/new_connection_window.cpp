0007e659930037200f81babefea0677693eba4c1
Xu Shaohua <shaohua@biofan.org>
2021-04-04 01:16:11 +0800

    frames: Add new connection window

---
+++ src/frames/new_connection_window.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+#include "frames/new_connection_window.h"
+
+namespace hebo {
+
+NewConnectionWindow::NewConnectionWindow(QWidget* parent) : QWidget(parent) {
+
+}
+
+}  // namespace hebo
