249c435921115703f2f7862bdda11215e47a3d3b
Xu Shaohua <shaohua@biofan.org>
2021-04-08 08:37:56 +0800

    widgets: Add color channel line edit

---
+++ src/widgets/color_channel_line_edit.cpp
@@ -0,0 +1,70 @@
+// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+#include "widgets/color_channel_line_edit.h"
+
+#include <QIntValidator>
+#include <QKeyEvent>
+#include <QPainter>
+
+#include "base/file.h"
+#include "resources/styles/styles.h"
+
+namespace hebo {
+namespace {
+
+constexpr int kMinValue = 0;
+constexpr int kMaxValue = 255;
+
+}  // namespace
+
+ColorChannelLineEdit::ColorChannelLineEdit(QWidget* parent) : QLineEdit(parent) {
+  auto* validator = new QIntValidator(kMinValue, kMaxValue, this);
+  this->setValidator(validator);
+  this->setFixedWidth(36);
+
+  this->setStyleSheet(readTextFile(kStyleColorLineEdit));
+
+  connect(this, &ColorChannelLineEdit::editingFinished,
+          this, &ColorChannelLineEdit::onTextChanged);
+}
+
+int ColorChannelLineEdit::value() const {
+  return text().toInt();
+}
+
+void ColorChannelLineEdit::setValue(int value) {
+  QSignalBlocker blocker(this);
+  this->setText(QString::number(value));
+}
+
+void ColorChannelLineEdit::onTextChanged() {
+  emit this->valueChanged(this->value());
+}
+
+void ColorChannelLineEdit::keyPressEvent(QKeyEvent* event) {
+  switch (event->key()) {
+    case Qt::Key_Up: {
+      const int new_value = this->value() + 1;
+      if (new_value <= kMaxValue) {
+        this->setValue(new_value);
+        emit this->valueChanged(new_value);
+      }
+      break;
+    }
+    case Qt::Key_Down: {
+      const int new_value = this->value() - 1;
+      if (new_value >= kMinValue) {
+        this->setValue(new_value);
+        emit this->valueChanged(new_value);
+      }
+      break;
+    }
+    default: {
+      QLineEdit::keyPressEvent(event);
+    }
+  }
+}
+
+}  // namespace hebo
---
+++ src/widgets/color_channel_line_edit.h
@@ -0,0 +1,36 @@
+// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+#ifndef HEBO_SRC_WIDGETS_COLOR_CHANNEL_LINE_EDIT_H_
+#define HEBO_SRC_WIDGETS_COLOR_CHANNEL_LINE_EDIT_H_
+
+#include <QLineEdit>
+
+namespace hebo {
+
+class ColorChannelLineEdit : public QLineEdit {
+  Q_OBJECT
+  Q_PROPERTY(int value READ value WRITE setValue NOTIFY valueChanged)
+
+ public:
+  explicit ColorChannelLineEdit(QWidget* parent = nullptr);
+
+  [[nodiscard]] int value() const;
+
+ public slots:
+  void setValue(int value);
+
+ signals:
+  void valueChanged(int value);
+
+ protected:
+  void keyPressEvent(QKeyEvent* event) override;
+
+ private slots:
+  void onTextChanged();
+};
+
+}  // namespace hebo
+
+#endif  // HEBO_SRC_WIDGETS_COLOR_CHANNEL_LINE_EDIT_H_
