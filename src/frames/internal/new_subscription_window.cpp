606d5bb4d4803d2bf96960f7b94d144f9cfc0a22
Xu Shaohua <shaohua@biofan.org>
2021-04-08 13:57:38 +0800

    newSubWindow: Add content

--- src/frames/internal/new_subscription_window.cpp
+++ src/frames/internal/new_subscription_window.cpp
@@ -4,10 +4,59 @@
 
 #include "frames/internal/new_subscription_window.h"
 
+#include <QFormLayout>
+#include <QLabel>
+
+#include "resources/fonts/fonts.h"
+
 namespace hebo {
 
 NewSubscriptionWindow::NewSubscriptionWindow(QWidget* parent) : QFrame(parent) {
+  this->initUi();
+  this->initSignals();
+}
 
+void NewSubscriptionWindow::initUi() {
+  this->setWindowTitle(tr("New Subscription"));
+
+  auto* main_layout = new QVBoxLayout();
+  this->setLayout(main_layout);
+
+  auto* form_layout = new QFormLayout();
+  main_layout->addLayout(form_layout);
+
+  this->topic_edit_ = new QLineEdit();
+  form_layout->addRow(new QLabel(tr("Topic")), this->topic_edit_);
+
+  this->qos_box_ = new QComboBox();
+  this->qos_model_ = new QoSModel(this);
+  this->qos_box_->setModel(this->qos_model_);
+  form_layout->addRow(new QLabel(tr("QoS")), this->qos_box_);
+
+  auto* color_layout = new QHBoxLayout();
+  this->color_edit_ = new ColorLineEdit();
+  color_layout->addWidget(this->color_edit_);
+  this->refresh_color_button_ = new FontIconButton(kFontElIconRefresh);
+  color_layout->addWidget(this->refresh_color_button_);
+  form_layout->addRow(new QLabel(tr("Color")), color_layout);
+
+  this->alias_edit_ = new QLineEdit();
+  form_layout->addRow(new QLabel(tr("Alias")), this->alias_edit_);
+
+  auto* buttons_layout = new QHBoxLayout();
+  main_layout->addSpacing(12);
+  main_layout->addLayout(buttons_layout);
+  this->cancel_button_ = new QPushButton(tr("Cancel"));
+  this->ok_button_ = new QPushButton(tr("Ok"));
+  buttons_layout->addWidget(this->cancel_button_);
+  buttons_layout->addWidget(this->ok_button_);
+}
+
+void NewSubscriptionWindow::initSignals() {
+  connect(this->cancel_button_, &QPushButton::clicked,
+          this, &NewSubscriptionWindow::hide);
+  connect(this->ok_button_, &QPushButton::clicked,
+          this, &NewSubscriptionWindow::confirmed);
 }
 
 }  // namespace hebo
--- src/frames/internal/new_subscription_window.h
+++ src/frames/internal/new_subscription_window.h
@@ -5,7 +5,14 @@
 #ifndef HEBO_SRC_FRAMES_INTERNAL_NEW_SUBSCRIPTION_WINDOW_H_
 #define HEBO_SRC_FRAMES_INTERNAL_NEW_SUBSCRIPTION_WINDOW_H_
 
+#include <QComboBox>
+#include <QMessageBox>
 #include <QFrame>
+#include <QLineEdit>
+
+#include "frames/models/qos_model.h"
+#include "widgets/color_line_edit.h"
+#include "widgets/font_icon_button.h"
 
 namespace hebo {
 
@@ -13,6 +20,31 @@ class NewSubscriptionWindow : public QFrame {
   Q_OBJECT
  public:
   explicit NewSubscriptionWindow(QWidget* parent = nullptr);
+
+  [[nodiscard]] QString topic() const { return this->topic_edit_->text(); }
+  [[nodiscard]] QoS qos() const {
+    const auto index = this->qos_model_->index(this->qos_box_->currentIndex(), 0);
+    return this->qos_model_->data(index, QoSModel::kIdRole).value<QoS>();
+  }
+  [[nodiscard]] QColor color() const { return this->color_edit_->color(); }
+  [[nodiscard]] QString alias() const { return this->alias_edit_->text(); }
+
+ signals:
+  void confirmed();
+
+ private:
+  void initUi();
+  void initSignals();
+
+  QLineEdit* topic_edit_{nullptr};
+  QComboBox* qos_box_{nullptr};
+  QoSModel* qos_model_{nullptr};
+  ColorLineEdit* color_edit_{nullptr};
+  FontIconButton* refresh_color_button_{nullptr};
+  QLineEdit* alias_edit_{nullptr};
+
+  QPushButton* cancel_button_{nullptr};
+  QPushButton* ok_button_{nullptr};
 };
 
 }  // namespace hebo
