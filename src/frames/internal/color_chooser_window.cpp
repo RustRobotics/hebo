5d736117ab5616fd76ee08e3d6c474250df1b7d6
Xu Shaohua <shaohua@biofan.org>
2021-04-08 08:54:37 +0800

    colorChooserWindow: Simplify layout

--- src/frames/internal/color_chooser_window.cpp
+++ src/frames/internal/color_chooser_window.cpp
@@ -25,55 +25,9 @@ ColorChooserWindow::ColorChooserWindow(QWidget* parent) : QWidget(parent) {
 void ColorChooserWindow::initUi() {
   auto* main_layout = new QVBoxLayout();
   main_layout->setContentsMargins(0, 0, 0, 0);
-  main_layout->setSpacing(0);
-  this->setLayout(main_layout);
-
-  auto* group = new QButtonGroup(this);
-  auto* button_layout = new QHBoxLayout();
-  button_layout->setContentsMargins(0, 0, 0, 0);
-  button_layout->setSpacing(0);
-  this->button_container_ = new QWidget();
-  main_layout->addWidget(this->button_container_);
-  this->button_container_->setLayout(button_layout);
-
-  this->solid_button_ = new FlatButton(tr("Solid"));
-  this->solid_button_->setCheckable(true);
-  this->solid_button_->setChecked(true);
-  this->gradient_button_ = new FlatButton(tr("Gradient"));
-  this->gradient_button_->setCheckable(true);
-  group->addButton(this->solid_button_);
-  group->addButton(this->gradient_button_);
-  button_layout->addWidget(this->solid_button_);
-  button_layout->addWidget(this->gradient_button_);
-
-  this->stacked_layout_ = new QStackedLayout();
-  main_layout->addLayout(this->stacked_layout_);
-
-  this->initSolidColor();
-  this->initGradient();
-
-  this->setFixedWidth(290);
-}
-
-void ColorChooserWindow::initSolidColor() {
-  this->solid_page_ = new QWidget();
-  auto* main_layout = new QVBoxLayout();
-  main_layout->setContentsMargins(9, 9, 9, 9);
   main_layout->setSpacing(6);
-  this->solid_page_->setLayout(main_layout);
-  this->stacked_layout_->addWidget(this->solid_page_);
-
-  auto* button_layout = new QHBoxLayout();
-  button_layout->setSpacing(10);
-  button_layout->setContentsMargins(0, 0, 0, 0);
-  main_layout->addLayout(button_layout);
-
-  this->color_chooser_button_ = new FlatButton(tr("Color Picker"));
-  this->color_chooser_button_->setIcon(QPixmap(kImageActionColorPicker));
-  button_layout->addWidget(this->color_chooser_button_);
-  this->transparent_button_ = new FlatButton(tr("Transparent"));
-  this->transparent_button_->setIcon(QPixmap(kImageActionColorTransparent));
-  button_layout->addWidget(this->transparent_button_);
+  this->setLayout(main_layout);
+  this->setFixedWidth(290);
 
   this->color_picker_ = new HSVColorPicker();
   main_layout->addWidget(this->color_picker_);
@@ -84,83 +38,19 @@ void ColorChooserWindow::initSolidColor() {
   main_layout->addLayout(grid_layout);
   this->color_line_edit_ = new ColorLineEdit();
   grid_layout->addWidget(this->color_line_edit_, 0, 0);
-  this->r_line_edit_ = new ColorChannelLineEdit();
-  grid_layout->addWidget(this->r_line_edit_, 0, 1);
-  this->g_line_edit_ = new ColorChannelLineEdit();
-  grid_layout->addWidget(this->g_line_edit_, 0, 2);
-  this->b_line_edit_ = new ColorChannelLineEdit();
-  grid_layout->addWidget(this->b_line_edit_, 0, 3);
-  this->a_line_edit_ = new ColorChannelLineEdit();
-  grid_layout->addWidget(this->a_line_edit_, 0, 4);
-
-  auto* hex_label = new ColorLabel(tr("Hex"));
-  grid_layout->addWidget(hex_label, 1, 0);
-  auto* r_label = new ColorLabel(tr("R"));
-  grid_layout->addWidget(r_label, 1, 1);
-  auto* g_label = new ColorLabel(tr("G"));
-  grid_layout->addWidget(g_label, 1, 2);
-  auto* b_label = new ColorLabel(tr("B"));
-  grid_layout->addWidget(b_label, 1, 3);
-  this->a_label_ = new ColorLabel(tr("A"));
-  grid_layout->addWidget(this->a_label_);
-
   main_layout->addWidget(new SeparatorLine(Qt::Horizontal));
 
   this->color_palette_list_view_ = new ColorPaletteListView();
   main_layout->addWidget(this->color_palette_list_view_);
 }
 
-void ColorChooserWindow::initGradient() {
-  this->gradient_page_ = new QWidget();
-  this->stacked_layout_->addWidget(this->gradient_page_);
-  auto* main_layout = new QVBoxLayout();
-  main_layout->setSpacing(0);
-  main_layout->setContentsMargins(0, 0, 0, 0);
-  this->gradient_page_->setLayout(main_layout);
-}
-
 void ColorChooserWindow::initSignals() {
-  connect(this->solid_button_, &FlatButton::clicked, [=]() {
-    this->stacked_layout_->setCurrentIndex(0);
-  });
-  connect(this->gradient_button_, &FlatButton::clicked, [=]() {
-    this->stacked_layout_->setCurrentIndex(1);
-  });
-
-  connect(this->transparent_button_, &FlatButton::clicked, [=]() {
-    this->solid_color_.setAlpha(0);
-    this->updateColorEdit(this->solid_color_);
-  });
-
   connect(this->color_palette_list_view_, &ColorPaletteListView::colorChanged,
           this, &ColorChooserWindow::updateColorEdit);
   connect(this->color_picker_, &HSVColorPicker::colorChanged,
           this, &ColorChooserWindow::updateColorEdit);
   connect(this->color_line_edit_, &ColorLineEdit::colorChanged,
           this, &ColorChooserWindow::updateColorEdit);
-  connect(this->r_line_edit_, &ColorChannelLineEdit::valueChanged, [=](int value) {
-    this->solid_color_.setRed(value);
-    this->updateColorEdit(this->solid_color_);
-  });
-  connect(this->g_line_edit_, &ColorChannelLineEdit::valueChanged, [=](int value) {
-    this->solid_color_.setGreen(value);
-    this->updateColorEdit(this->solid_color_);
-  });
-  connect(this->b_line_edit_, &ColorChannelLineEdit::valueChanged, [=](int value) {
-    this->solid_color_.setBlue(value);
-    this->updateColorEdit(this->solid_color_);
-  });
-  connect(this->a_line_edit_, &ColorChannelLineEdit::valueChanged, [=](int value) {
-    this->solid_color_.setAlpha(value);
-    this->updateColorEdit(this->solid_color_);
-  });
-}
-
-void ColorChooserWindow::setGradientVisible(bool visible) {
-  this->button_container_->setVisible(visible);
-  if (!visible) {
-    this->solid_button_->setChecked(true);
-  }
 }
 
 void ColorChooserWindow::setSolidColorPalette(const ColorPalette& palette) {
@@ -180,18 +70,8 @@ void ColorChooserWindow::focusOutEvent(QFocusEvent* event) {
 void ColorChooserWindow::setColor(const QColor& color) {
   this->solid_color_ = color;
   this->color_line_edit_->setColor(color);
-  this->r_line_edit_->setValue(color.red());
-  this->g_line_edit_->setValue(color.green());
-  this->b_line_edit_->setValue(color.blue());
-  this->a_line_edit_->setValue(color.alpha());
   this->color_picker_->setColor(color);
   this->color_picker_->setPreviewColor(color);
-}
-
-void ColorChooserWindow::setEnableTransparent(bool enable) {
-  this->transparent_button_->setVisible(enable);
-  this->a_line_edit_->setVisible(enable);
-  this->a_label_->setVisible(enable);
 }
 
 }  // namespace hebo
--- src/frames/internal/color_chooser_window.h
+++ src/frames/internal/color_chooser_window.h
@@ -25,10 +25,6 @@ class ColorChooserWindow : public QWidget {
  public:
   explicit ColorChooserWindow(QWidget* parent = nullptr);
 
-  void setGradientVisible(bool visible);
-
-  void setEnableTransparent(bool enable);
-
   void setSolidColorPalette(const ColorPalette& palette);
 
   [[nodiscard]] const QColor& color() const { return this->solid_color_; }
@@ -49,30 +45,13 @@ class ColorChooserWindow : public QWidget {
 
  private:
   void initUi();
-  void initSolidColor();
-  void initGradient();
-
   void initSignals();
-
-  QStackedLayout* stacked_layout_{nullptr};
-
-  QWidget* button_container_{nullptr};
-  FlatButton* solid_button_{nullptr};
-  FlatButton* gradient_button_{nullptr};
 
   QWidget* solid_page_{nullptr};
   QColor solid_color_{};
   ColorPaletteListView* color_palette_list_view_{nullptr};
-  FlatButton* color_chooser_button_{nullptr};
-  FlatButton* transparent_button_{nullptr};
   HSVColorPicker* color_picker_{nullptr};
   ColorLineEdit* color_line_edit_{nullptr};
-  ColorChannelLineEdit* r_line_edit_{nullptr};
-  ColorChannelLineEdit* g_line_edit_{nullptr};
-  ColorChannelLineEdit* b_line_edit_{nullptr};
-  ColorChannelLineEdit* a_line_edit_{nullptr};
-
-  QWidget* gradient_page_{nullptr};
 
   ColorLabel* a_label_{nullptr};
 };
