2fc1f22b6d32a609957a53402edc68ca741d68ff
Xu Shaohua <shaohua@biofan.org>
2021-04-09 11:14:21 +0800

    Delete qml component

--- src/ui/ui.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
-// Use of this source is governed by General Public License that can be found
-// in the LICENSE file.
-
-namespace hebo {
-
-constexpr const char* kUiMainWindow = "qrc:///ui/MainWindow.qml";
-
-}  // namespace hebo
