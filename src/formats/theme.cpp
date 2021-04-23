77a41270013200780dba89afd134bccc311b8ae3
Xu Shaohua <shaohua@biofan.org>
2021-04-23 15:26:20 +0800

    formats: Add theme enum

---
+++ src/formats/theme.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+#include "formats/theme.h"
+
+namespace hebo {
+
+}  // namespace hebo
---
+++ src/formats/theme.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+#ifndef HEBO_SRC_FORMATS_THEME_H_
+#define HEBO_SRC_FORMATS_THEME_H_
+
+#include <cstdint>
+#include <QString>
+#include <QMetaType>
+
+namespace hebo {
+
+enum class ThemeType : uint8_t {
+  kDay,
+  kNight,
+};
+
+}  // namespace hebo
+
+Q_DECLARE_METATYPE(hebo::ThemeType);
+
+#endif  // HEBO_SRC_FORMATS_THEME_H_
