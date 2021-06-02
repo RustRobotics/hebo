6704b357906d805da14e2e2068d32e8d6250594b
Xu Shaohua <shaohua@biofan.org>
2021-06-02 14:19:29 +0800

    Fix rusty compile error

--- src/tests/qt_test_main.cpp
+++ src/tests/qt_test_main.cpp
@@ -2,14 +2,11 @@
 // Use of this source is governed by General Public License that can be found
 // in the LICENSE file.
 
-#include <ConsoleAppender.h>
-#include <Logger.h>
 #include <QGuiApplication>
 #include <gtest/gtest.h>
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
-  cuteLogger->registerAppender(new ConsoleAppender());
   QGuiApplication app(argc, argv);
   return RUN_ALL_TESTS();
 }
