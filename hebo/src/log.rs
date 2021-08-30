0d470cbd7b72f6ea336e638d9a0c2bf00c8efeac
Xu Shaohua <shaohua@biofan.org>
2021-08-30 10:07:26 +0800

    hebo: Log to file

--- hebo/src/log.rs
+++ hebo/src/log.rs
@@ -4,12 +4,18 @@
 
 use log::LevelFilter;
 use log4rs::append::console::ConsoleAppender;
-use log4rs::append::rolling_file::policy::compound::CompoundPolicy;
+use log4rs::append::rolling_file::policy::compound::{
+    roll::fixed_window::FixedWindowRoller, trigger::size::SizeTrigger, CompoundPolicy,
+};
 use log4rs::append::rolling_file::RollingFileAppender;
 use log4rs::config::{Appender, Config, Logger, Root};
 
 use crate::config;
 use crate::error::Error;
+
+const LOG_FILE_SIZE: u64 = 16 * 1024 * 1024;
+const ROLLER_PATTERN: &str = ".{}.gz";
+const ROLLER_COUNT: u32 = 10;
 
 fn get_log_level(level: config::LogLevel) -> LevelFilter {
     match level {
@@ -25,7 +31,15 @@ fn get_log_level(level: config::LogLevel) -> LevelFilter {
 pub fn init_log(log_conf: &config::Log) -> Result<(), Error> {
     let stdout = ConsoleAppender::builder().build();
 
-    let rolling_policy = CompoundPolicy::new();
+    let mut roller_pattern = log_conf.log_file.to_str().unwrap().to_string();
+    roller_pattern += ROLLER_PATTERN;
+    let roller = FixedWindowRoller::builder()
+        .build(&roller_pattern, ROLLER_COUNT)
+        .unwrap();
+    let rolling_policy = Box::new(CompoundPolicy::new(
+        Box::new(SizeTrigger::new(LOG_FILE_SIZE)),
+        Box::new(roller),
+    ));
     let requests = RollingFileAppender::builder()
         .build(&log_conf.log_file, rolling_policy)
         .unwrap();
