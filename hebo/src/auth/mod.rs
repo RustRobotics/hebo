d7c94c3827ff963856c09c24be7c7827d467346b
Xu Shaohua <shaohua@biofan.org>
2021-09-01 09:57:15 +0800

    Update copyright statement

--- hebo/src/auth/mod.rs
+++ hebo/src/auth/mod.rs
@@ -1,7 +1,3 @@
 // Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
 // Use of this source is governed by Affero General Public License that can be found
 // in the LICENSE file.
-
-pub const DEFAULT_CONFIG: &'static str = "/etc/hebo/hebo.toml";
-
-pub const CHANNEL_CAPACITY: usize = 16;
