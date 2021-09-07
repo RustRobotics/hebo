ba68c4417e02be1fc7e59c17f615bdfe652a6e90
Xu Shaohua <shaohua@biofan.org>
2021-09-07 09:43:41 +0800

    serverContext: Add init_modules()

--- src/server.rs
+++ src/server.rs
@@ -127,126 +127,128 @@ impl ServerContext {
     pub fn run_loop(&mut self, runtime: Runtime) -> Result<(), Error> {
         self.write_pid()?;
 
-        runtime.block_on(async {
-            let (listeners_to_dispatcher_sender, listeners_to_dispatcher_receiver) =
+        runtime.block_on(async { self.init_modules(&runtime).await });
+
+        Ok(())
+    }
+
+    async fn init_modules(&mut self, runtime: &Runtime) -> Result<(), Error> {
+        let (listeners_to_dispatcher_sender, listeners_to_dispatcher_receiver) =
+            mpsc::channel(CHANNEL_CAPACITY);
+        let mut dispatcher_to_listener_senders = Vec::new();
+        let (listeners_to_auth_sender, listeners_to_auth_receiver) =
+            mpsc::channel(CHANNEL_CAPACITY);
+        let mut auth_to_listener_senders = Vec::new();
+
+        let mut handles = Vec::new();
+        let mut listener_id: u32 = 0;
+        let mut listeners_info = Vec::new();
+
+        for l in self.config.listeners.clone() {
+            listeners_info.push((listener_id, l.address.clone()));
+            let (dispatcher_to_listener_sender, dispatcher_to_listener_receiver) =
                 mpsc::channel(CHANNEL_CAPACITY);
-            let mut dispatcher_to_listener_senders = Vec::new();
-            let (listeners_to_auth_sender, listeners_to_auth_receiver) =
+            dispatcher_to_listener_senders.push((listener_id, dispatcher_to_listener_sender));
+
+            let (auth_to_listener_sender, auth_to_listener_receiver) =
                 mpsc::channel(CHANNEL_CAPACITY);
-            let mut auth_to_listener_senders = Vec::new();
+            auth_to_listener_senders.push((listener_id, auth_to_listener_sender));
 
-            let mut handles = Vec::new();
-            let mut listener_id: u32 = 0;
-            let mut listeners_info = Vec::new();
-
-            for l in self.config.listeners.clone() {
-                listeners_info.push((listener_id, l.address.clone()));
-                let (dispatcher_to_listener_sender, dispatcher_to_listener_receiver) =
-                    mpsc::channel(CHANNEL_CAPACITY);
-                dispatcher_to_listener_senders.push((listener_id, dispatcher_to_listener_sender));
-
-                let (auth_to_listener_sender, auth_to_listener_receiver) =
-                    mpsc::channel(CHANNEL_CAPACITY);
-                auth_to_listener_senders.push((listener_id, auth_to_listener_sender));
-
-                let mut listener = Listener::bind(
-                    listener_id,
-                    l,
-                    listeners_to_dispatcher_sender.clone(),
-                    dispatcher_to_listener_receiver,
-                    listeners_to_auth_sender.clone(),
-                    auth_to_listener_receiver,
-                )
-                .await
-                .expect(&format!("Failed to listen at {:?}", &listeners_info.last()));
-                let handle = runtime.spawn(async move {
-                    listener.run_loop().await;
-                });
-                handles.push(handle);
-                listener_id += 1;
-            }
-
-            let (system_to_dispatcher_sender, system_to_dispatcher_receiver) =
-                mpsc::channel(CHANNEL_CAPACITY);
-            let (system_to_cache_sender, system_to_cache_receiver) =
-                mpsc::channel(CHANNEL_CAPACITY);
-            let (cache_to_system_sender, cache_to_system_receiver) =
-                mpsc::channel(CHANNEL_CAPACITY);
-
-            let mut system = System::new(
-                self.config.general.sys_interval,
-                system_to_dispatcher_sender,
-                system_to_cache_sender,
-                cache_to_system_receiver,
-            );
-            let system_handle = runtime.spawn(async move {
-                system.run_loop().await;
-            });
-            handles.push(system_handle);
-
-            let (cache_to_dispatcher_sender, cache_to_dispatcher_receiver) =
-                mpsc::channel(CHANNEL_CAPACITY);
-            let (dispatcher_to_cache_sender, dispatcher_to_cache_receiver) =
-                mpsc::channel(CHANNEL_CAPACITY);
-            let mut cache = Cache::new(
-                cache_to_dispatcher_sender,
-                dispatcher_to_cache_receiver,
-                cache_to_system_sender,
-                system_to_cache_receiver,
-            );
-            let cache_handle = runtime.spawn(async move {
-                cache.run_loop().await;
-            });
-            handles.push(cache_handle);
-
-            for listener_info in &listeners_info {
-                if let Err(err) = dispatcher_to_cache_sender
-                    .send(DispatcherToCacheCmd::ListenerAdded(
-                        listener_info.0,
-                        listener_info.1.clone(),
-                    ))
-                    .await
-                {
-                    log::error!(
-                        "Failed to send listener {:?} to cache, err: {:?}",
-                        listener_info.1,
-                        err
-                    );
-                }
-            }
-
-            // TODO(Shaohua): Returns an error.
-            let mut auth_app = AuthApp::new(
-                self.config.security.clone(),
-                auth_to_listener_senders,
-                listeners_to_auth_receiver,
+            let mut listener = Listener::bind(
+                listener_id,
+                l,
+                listeners_to_dispatcher_sender.clone(),
+                dispatcher_to_listener_receiver,
+                listeners_to_auth_sender.clone(),
+                auth_to_listener_receiver,
             )
-            .expect("Failed to init auth app");
-
-            let auth_app_handle = runtime.spawn(async move {
-                auth_app.run_loop().await;
+            .await
+            .expect(&format!("Failed to listen at {:?}", &listeners_info.last()));
+            let handle = runtime.spawn(async move {
+                listener.run_loop().await;
             });
-            handles.push(auth_app_handle);
+            handles.push(handle);
+            listener_id += 1;
+        }
 
-            let mut dispatcher = Dispatcher::new(
-                // listeners module
-                dispatcher_to_listener_senders,
-                listeners_to_dispatcher_receiver,
-                // system module
-                system_to_dispatcher_receiver,
-                // cache module
-                dispatcher_to_cache_sender,
-                cache_to_dispatcher_receiver,
-            );
-            let dispatcher_handle = runtime.spawn(async move {
-                dispatcher.run_loop().await;
-            });
-            handles.push(dispatcher_handle);
+        let (system_to_dispatcher_sender, system_to_dispatcher_receiver) =
+            mpsc::channel(CHANNEL_CAPACITY);
+        let (system_to_cache_sender, system_to_cache_receiver) = mpsc::channel(CHANNEL_CAPACITY);
+        let (cache_to_system_sender, cache_to_system_receiver) = mpsc::channel(CHANNEL_CAPACITY);
 
-            for handle in handles {
-                let _ret = handle.await;
-            }
+        let mut system = System::new(
+            self.config.general.sys_interval,
+            system_to_dispatcher_sender,
+            system_to_cache_sender,
+            cache_to_system_receiver,
+        );
+        let system_handle = runtime.spawn(async move {
+            system.run_loop().await;
         });
+        handles.push(system_handle);
+
+        let (cache_to_dispatcher_sender, cache_to_dispatcher_receiver) =
+            mpsc::channel(CHANNEL_CAPACITY);
+        let (dispatcher_to_cache_sender, dispatcher_to_cache_receiver) =
+            mpsc::channel(CHANNEL_CAPACITY);
+        let mut cache = Cache::new(
+            cache_to_dispatcher_sender,
+            dispatcher_to_cache_receiver,
+            cache_to_system_sender,
+            system_to_cache_receiver,
+        );
+        let cache_handle = runtime.spawn(async move {
+            cache.run_loop().await;
+        });
+        handles.push(cache_handle);
+
+        for listener_info in &listeners_info {
+            if let Err(err) = dispatcher_to_cache_sender
+                .send(DispatcherToCacheCmd::ListenerAdded(
+                    listener_info.0,
+                    listener_info.1.clone(),
+                ))
+                .await
+            {
+                log::error!(
+                    "Failed to send listener {:?} to cache, err: {:?}",
+                    listener_info.1,
+                    err
+                );
+            }
+        }
+
+        // TODO(Shaohua): Returns an error.
+        let mut auth_app = AuthApp::new(
+            self.config.security.clone(),
+            auth_to_listener_senders,
+            listeners_to_auth_receiver,
+        )
+        .expect("Failed to init auth app");
+
+        let auth_app_handle = runtime.spawn(async move {
+            auth_app.run_loop().await;
+        });
+        handles.push(auth_app_handle);
+
+        let mut dispatcher = Dispatcher::new(
+            // listeners module
+            dispatcher_to_listener_senders,
+            listeners_to_dispatcher_receiver,
+            // system module
+            system_to_dispatcher_receiver,
+            // cache module
+            dispatcher_to_cache_sender,
+            cache_to_dispatcher_receiver,
+        );
+        let dispatcher_handle = runtime.spawn(async move {
+            dispatcher.run_loop().await;
+        });
+        handles.push(dispatcher_handle);
+
+        for handle in handles {
+            let _ret = handle.await;
+        }
 
         Ok(())
     }
