269de96aeeaaa6209584fd4e1dde48ec6ad94e89
Xu Shaohua <shaohua@biofan.org>
2021-09-16 09:57:56 +0800

    dispatcher: Move handlers to separated modules

---
+++ src/dispatcher/listener.rs
@@ -0,0 +1,43 @@
+// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Affero General Public License that can be found
+// in the LICENSE file.
+
+use codec::PublishPacket;
+
+use super::Dispatcher;
+use crate::commands::{DispatcherToListenerCmd, ListenerToDispatcherCmd};
+
+impl Dispatcher {
+    pub(super) async fn handle_listener_cmd(&mut self, cmd: ListenerToDispatcherCmd) {
+        log::info!("handle_listener_cmd: {:?}", cmd);
+        match cmd {
+            ListenerToDispatcherCmd::Publish(packet) => {
+                self.backends_store_packet(&packet).await;
+                self.publish_packet_to_listners(&packet).await;
+            }
+            ListenerToDispatcherCmd::SessionAdded(listener_id) => {
+                self.metrics_on_session_added(listener_id).await;
+            }
+            ListenerToDispatcherCmd::SessionRemoved(listener_id) => {
+                self.metrics_on_session_removed(listener_id).await;
+            }
+            ListenerToDispatcherCmd::SubscriptionsAdded(listener_id) => {
+                self.metrics_on_subscription_added(listener_id).await;
+            }
+            ListenerToDispatcherCmd::SubscriptionsRemoved(listener_id) => {
+                self.metrics_on_subscription_removed(listener_id).await;
+            }
+        }
+    }
+    pub(super) async fn publish_packet_to_listners(&mut self, packet: &PublishPacket) {
+        for (_listener_id, sender) in &self.listener_senders {
+            let cmd = DispatcherToListenerCmd::Publish(packet.clone());
+            if let Err(err) = sender.send(cmd).await {
+                log::error!(
+                    "Dispatcher::publish_packet_to_listener() send failed: {:?}",
+                    err
+                );
+            }
+        }
+    }
+}
--- src/dispatcher/mod.rs
+++ src/dispatcher/mod.rs
@@ -2,7 +2,6 @@
 // Use of this source is governed by Affero General Public License that can be found
 // in the LICENSE file.
 
-use codec::PublishPacket;
 use tokio::sync::mpsc::{Receiver, Sender};
 
 use crate::commands::{
@@ -12,6 +11,13 @@ use crate::commands::{
     MetricsToDispatcherCmd, RuleEngineToDispatcherCmd,
 };
 use crate::types::ListenerId;
+
+mod backends;
+mod bridge;
+mod gateway;
+mod listener;
+mod metrics;
+mod rule_engine;
 
 /// Dispatcher is a message router.
 #[derive(Debug)]
@@ -99,148 +105,5 @@ impl Dispatcher {
                 },
             }
         }
-    }
-
-    async fn handle_metrics_cmd(&mut self, cmd: MetricsToDispatcherCmd) {
-        //log::info!("handle metrics cmd: {:?}", cmd);
-        match cmd {
-            MetricsToDispatcherCmd::Publish(packet) => {
-                self.publish_packet_to_listners(&packet).await;
-            }
-        }
-    }
-
-    async fn handle_listener_cmd(&mut self, cmd: ListenerToDispatcherCmd) {
-        log::info!("handle_listener_cmd: {:?}", cmd);
-        match cmd {
-            ListenerToDispatcherCmd::Publish(packet) => {
-                self.backends_store_packet(&packet).await;
-                self.publish_packet_to_listners(&packet).await;
-            }
-            ListenerToDispatcherCmd::SessionAdded(listener_id) => {
-                self.metrics_on_session_added(listener_id).await;
-            }
-            ListenerToDispatcherCmd::SessionRemoved(listener_id) => {
-                self.metrics_on_session_removed(listener_id).await;
-            }
-            ListenerToDispatcherCmd::SubscriptionsAdded(listener_id) => {
-                self.metrics_on_subscription_added(listener_id).await;
-            }
-            ListenerToDispatcherCmd::SubscriptionsRemoved(listener_id) => {
-                self.metrics_on_subscription_removed(listener_id).await;
-            }
-        }
-    }
-
-    /// Send packet to backends.
-    async fn backends_store_packet(&mut self, packet: &PublishPacket) {
-        log::info!("backends store packet: {:?}", packet);
-    }
-
-    async fn publish_packet_to_listners(&mut self, packet: &PublishPacket) {
-        for (_listener_id, sender) in &self.listener_senders {
-            let cmd = DispatcherToListenerCmd::Publish(packet.clone());
-            if let Err(err) = sender.send(cmd).await {
-                log::error!(
-                    "Dispatcher::publish_packet_to_listener() send failed: {:?}",
-                    err
-                );
-            }
-        }
-    }
-
-    // Metrics related methods
-    async fn metrics_publish_packet_sent(
-        &mut self,
-        listener_id: ListenerId,
-        count: usize,
-        bytes: usize,
-    ) {
-        if let Err(err) = self
-            .metrics_sender
-            .send(DispatcherToMetricsCmd::PublishPacketSent(
-                listener_id,
-                count,
-                bytes,
-            ))
-            .await
-        {
-            log::error!(
-                "Dispatcher: Failed to send UpdatePublishPacket, err: {:?}",
-                err
-            );
-        }
-    }
-
-    async fn metrics_on_session_added(&mut self, listener_id: ListenerId) {
-        if let Err(err) = self
-            .metrics_sender
-            .send(DispatcherToMetricsCmd::SessionAdded(listener_id, 1))
-            .await
-        {
-            log::error!(
-                "Dispatcher: Failed to send SessionAdded cmd, err: {:?}",
-                err
-            );
-        }
-    }
-
-    async fn metrics_on_session_removed(&mut self, listener_id: ListenerId) {
-        if let Err(err) = self
-            .metrics_sender
-            .send(DispatcherToMetricsCmd::SessionRemoved(listener_id, 1))
-            .await
-        {
-            log::error!(
-                "Dispatcher: Failed to send SessionRemoved cmd, err: {:?}",
-                err
-            );
-        }
-    }
-
-    async fn metrics_on_subscription_added(&mut self, listener_id: ListenerId) {
-        if let Err(err) = self
-            .metrics_sender
-            .send(DispatcherToMetricsCmd::SubscriptionsAdded(listener_id, 1))
-            .await
-        {
-            log::error!(
-                "Dispatcher: Failed to send SubscriptionsAdded cmd, err: {:?}",
-                err
-            );
-        }
-    }
-
-    async fn metrics_on_subscription_removed(&mut self, listener_id: ListenerId) {
-        if let Err(err) = self
-            .metrics_sender
-            .send(DispatcherToMetricsCmd::SubscriptionsRemoved(listener_id, 1))
-            .await
-        {
-            log::error!(
-                "Dispatcher: Failed to send SubscriptionsRemoved cmd, err: {:?}",
-                err
-            );
-        }
-    }
-
-    /// Backends app handlers
-    async fn handle_backends_cmd(&mut self, cmd: BackendsToDispatcherCmd) {
-        log::info!("cmd: {:?}", cmd);
-    }
-
-    /// Bridge app handlers
-    async fn handle_bridge_cmd(&mut self, cmd: BridgeToDispatcherCmd) {
-        log::info!("cmd: {:?}", cmd);
-    }
-
-    /// Gateway app handler
-    async fn handle_gateway_cmd(&mut self, cmd: GatewayToDispatcherCmd) {
-        log::info!("cmd: {:?}", cmd);
-    }
-
-    /// RuleEngine app handler
-    async fn handle_rule_engine_cmd(&mut self, cmd: RuleEngineToDispatcherCmd) {
-        log::info!("cmd: {:?}", cmd);
     }
 }
