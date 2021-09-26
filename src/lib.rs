9fb8f71ae97e88c994cad3ad60e429142eb5de81
Xu Shaohua <shaohua@biofan.org>
2021-09-26 16:11:25 +0800

    Move packets to v3/

--- src/lib.rs
+++ src/lib.rs
@@ -4,42 +4,16 @@
 
 pub mod base;
 pub mod byte_array;
-pub mod connect_ack_packet;
-pub mod connect_packet;
 pub mod consts;
-pub mod disconnect_packet;
 pub mod error;
 pub mod header;
-pub mod ping_request_packet;
-pub mod ping_response_packet;
-pub mod publish_ack_packet;
-pub mod publish_complete_packet;
-pub mod publish_packet;
-pub mod publish_received_packet;
-pub mod publish_release_packet;
-pub mod subscribe_ack_packet;
-pub mod subscribe_packet;
 pub mod topic;
-pub mod unsubscribe_ack_packet;
-pub mod unsubscribe_packet;
 pub mod utils;
 
 pub use base::{DecodePacket, EncodePacket, Packet, PacketId, QoS};
 pub use byte_array::ByteArray;
-pub use connect_ack_packet::{ConnectAckPacket, ConnectReturnCode};
-pub use connect_packet::ConnectPacket;
-pub use disconnect_packet::DisconnectPacket;
 pub use error::{DecodeError, EncodeError};
 pub use header::{FixedHeader, PacketType, RemainingLength};
-pub use ping_request_packet::PingRequestPacket;
-pub use ping_response_packet::PingResponsePacket;
-pub use publish_ack_packet::PublishAckPacket;
-pub use publish_complete_packet::PublishCompletePacket;
-pub use publish_packet::PublishPacket;
-pub use publish_received_packet::PublishReceivedPacket;
-pub use publish_release_packet::PublishReleasePacket;
-pub use subscribe_ack_packet::{SubscribeAck, SubscribeAckPacket};
-pub use subscribe_packet::{SubscribePacket, SubscribeTopic};
 pub use topic::{SubscribePattern, Topic, TopicError, TopicPart};
-pub use unsubscribe_ack_packet::UnsubscribeAckPacket;
-pub use unsubscribe_packet::UnsubscribePacket;
+
+pub use v3;
