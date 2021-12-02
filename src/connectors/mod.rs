ee0f0874904268072289a063992e43014e206065
Xu Shaohua <shaohua@biofan.org>
2021-12-02 18:20:07 +0800

    cargo: Make database connections optional

--- src/connectors/mod.rs
+++ src/connectors/mod.rs
@@ -2,7 +2,14 @@
 // Use of this source is governed by Affero General Public License that can be found
 // in the LICENSE file.
 
+#[cfg(feature = "mongodb_conn")]
 pub mod mongo_conn;
+
+#[cfg(feature = "mysql_conn")]
 pub mod mysql_conn;
+
+#[cfg(feature = "pgsql_conn")]
 pub mod pgsql_conn;
+
+#[cfg(feature = "redis_conn")]
 pub mod redis_conn;
