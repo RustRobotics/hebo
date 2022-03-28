96544ee75111d44d2828cf480c598f9cc471708e
Xu Shaohua <shaohua@biofan.org>
2022-03-28 13:38:27 +0800

    codec: Implements ops for u16data

--- codec/src/u16_data.rs
+++ codec/src/u16_data.rs
@@ -3,7 +3,10 @@
 // in the LICENSE file.
 
 use byteorder::{BigEndian, WriteBytesExt};
+use std::cmp;
+use std::convert;
 use std::fmt;
+use std::ops;
 
 use crate::{ByteArray, DecodeError, DecodePacket, EncodeError, EncodePacket};
 
@@ -46,5 +49,23 @@ impl EncodePacket for U16Data {
     fn encode(&self, buf: &mut Vec<u8>) -> Result<usize, EncodeError> {
         buf.write_u16::<BigEndian>(self.0)?;
         Ok(self.bytes())
+    }
+}
+
+impl ops::AddAssign<u16> for U16Data {
+    fn add_assign(&mut self, value: u16) {
+        self.0 += value;
+    }
+}
+
+impl cmp::PartialEq<u16> for U16Data {
+    fn eq(&self, value: &u16) -> bool {
+        self.0 == *value
+    }
+}
+
+impl convert::From<u16> for U16Data {
+    fn from(value: u16) -> Self {
+        Self::new(value)
     }
 }
