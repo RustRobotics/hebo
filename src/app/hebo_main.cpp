4a740b0cc62cf8f7241d2ee1c6b33f68ff691808
Xu Shaohua <shaohua@biofan.org>
2022-04-02 14:07:08 +0800

    Remove deprecated ui in cpp

--- src/app/hebo_main.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
-// Use of this source is governed by General Public License that can be found
-// in the LICENSE file.
-
-#include "controllers/shell.h"
-
-int main(int argc, char** argv) {
-  return hebo::runShell(argc, argv);
-}
