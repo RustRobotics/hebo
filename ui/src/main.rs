289971da776b831908fa0cbbd79db8cc54fff572
Xu Shaohua <shaohua@biofan.org>
2022-04-02 14:11:10 +0800

    ui: Add yew model

---
+++ ui/src/main.rs
@@ -0,0 +1,70 @@
+use yew::prelude::*;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Msg {
+    AddOne,
+    ShowAlert,
+}
+
+struct Model {
+    // `ComponentLink` is like a reference to a component.
+    // It can be used to send messages to the component
+    link: ComponentLink<Self>,
+    value: i64,
+}
+
+impl Model {
+    fn alert(&mut self) {
+        let window = web_sys::window().expect("Window not available");
+        window
+            .alert_with_message("hello, from wasm!")
+            .expect("alert failed!");
+    }
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        log::info!("hello, Model::create()");
+        Self { link, value: 0 }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                // the value has changed so we need to
+                // re-render for it to appear on the page
+                true
+            }
+            Msg::ShowAlert => {
+                self.alert();
+                false
+            }
+        }
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        // Should only return "true" if new properties are different to
+        // previously received properties.
+        // This component has no properties so we will always return "false".
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
+                <p>{ self.value }</p>
+                <p><button onclick=self.link.callback(|_| Msg::ShowAlert)>{ "Alert" }</button></p>
+            </div>
+        }
+    }
+}
+
+fn main() {
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::start_app::<Model>();
+}
