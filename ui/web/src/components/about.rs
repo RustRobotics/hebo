f302b67afe9bf21294fb84951402825f805bb2a7
Xu Shaohua <shaohua@biofan.org>
2022-04-11 10:48:20 +0800

    components: Add about component

--- ui/web/src/components/about.rs
+++ ui/web/src/components/about.rs
@@ -1,3 +1,22 @@
 // Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
+
+use yew::{html, Component, Context, Html};
+
+pub struct AboutComponent {}
+
+impl Component for AboutComponent {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {}
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        html! {
+           <div>{"Hello, About page."}</div>
+        }
+    }
+}
