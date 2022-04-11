0d3ed799600667d44542a1b2c0cb7d5879f17d37
Xu Shaohua <shaohua@biofan.org>
2022-04-11 14:15:24 +0800

    web: Add router

--- ui/web/src/app.rs
+++ ui/web/src/app.rs
@@ -2,47 +2,65 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
+use crate::components::about::AboutComponent;
+use crate::components::connections::ConnectionsComponent;
+use crate::components::log::LogComponent;
+use crate::components::new_connection::NewConnectionComponent;
+use crate::components::settings::SettingsComponent;
 use yew::prelude::{html, Component, Context, Html};
+use yew_router::prelude::*;
 
-use crate::client;
+#[derive(Debug, Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/connections")]
+    Connections,
 
-pub enum AppMsg {
-    SayHello,
-    SayHelloReturns(bool),
+    #[at("/new")]
+    NewConnection,
+
+    #[at("/log")]
+    Log,
+
+    #[at("/about")]
+    About,
+
+    #[at("/settings")]
+    Settings,
+
+    #[at("/")]
+    Home,
 }
 
 pub struct AppModel {}
 
+fn switch(routes: &Route) -> Html {
+    match routes {
+        Route::Home => html! { <ConnectionsComponent/> },
+        Route::Connections => html! { <ConnectionsComponent/> },
+        Route::NewConnection => html! { <NewConnectionComponent/> },
+        Route::Log => html! { <LogComponent/> },
+        Route::About => html! { <AboutComponent/> },
+        Route::Settings => html! { <SettingsComponent/> },
+    }
+}
+
 impl Component for AppModel {
-    type Message = AppMsg;
+    type Message = ();
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
         Self {}
     }
 
-    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            AppMsg::SayHello => {
-                ctx.link().send_future(async move {
-                    client::say_hello().await;
-                    AppMsg::SayHelloReturns(true)
-                });
-                true
-            }
-            AppMsg::SayHelloReturns(state) => {
-                log::info!("resp state: {}", state);
-                true
-            }
-        }
+    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
+        false
     }
 
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        let link = ctx.link();
+    fn view(&self, _ctx: &Context<Self>) -> Html {
         html! {
-            <div>
-                <button onclick={link.callback(|_| AppMsg::SayHello)}>{ "Hello" }</button>
-            </div>
+            <BrowserRouter>
+                <Switch<Route> render={Switch::render(switch)} />
+            </BrowserRouter>
         }
     }
 }
