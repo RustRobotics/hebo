eb0ca7616727545d32e613681e06c94577d4b661
Xu Shaohua <shaohua@biofan.org>
2022-04-25 11:33:25 +0800

    hebo: Fix port already in use for tests

--- hebo/tests/01-connect-allow-anonymous.rs
+++ hebo/tests/01-connect-allow-anonymous.rs
@@ -2,47 +2,78 @@
 // Use of this source is governed by Affero General Public License that can be found
 // in the LICENSE file.
 
+use hebo::error::Error;
 use std::thread::sleep;
 use std::time::Duration;
 
 mod common;
-use common::{Error, Server, ServerConfig};
+use common::{Server, ServerConfig};
 
-const ALLOW_CONFIG: &str = r#"
+const ALLOW_CONFIG_V4: &str = r#"
 [general]
-pid_file = "/tmp/hebo-tests/mqtt-1888.pid"
+pid_file = "/tmp/hebo-tests/mqtt-1889.pid"
 
 [[listeners]]
 protocol = "mqtt"
-address = "0.0.0.0:1888"
+address = "0.0.0.0:1889"
 
 [security]
 allow_anonymous = true
 
 [log]
-log_file = "/tmp/hebo-tests/hebo-1888.log"
+log_file = "/tmp/hebo-tests/hebo-1889.log"
 "#;
 
-const DENY_CONFIG: &str = r#"
+const ALLOW_CONFIG_V5: &str = r#"
 [general]
-pid_file = "/tmp/hebo-tests/mqtt-1888.pid"
+pid_file = "/tmp/hebo-tests/mqtt-1890.pid"
 
 [[listeners]]
 protocol = "mqtt"
-address = "0.0.0.0:1888"
+address = "0.0.0.0:1890"
+
+[security]
+allow_anonymous = true
+
+[log]
+log_file = "/tmp/hebo-tests/hebo-1890.log"
+"#;
+
+const DENY_CONFIG_V4: &str = r#"
+[general]
+pid_file = "/tmp/hebo-tests/mqtt-1891.pid"
+
+[[listeners]]
+protocol = "mqtt"
+address = "0.0.0.0:1891"
 
 [security]
 allow_anonymous = false
 
 [log]
-log_file = "/tmp/hebo-tests/hebo-1888.log"
+log_file = "/tmp/hebo-tests/hebo-1891.log"
+"#;
+
+const DENY_CONFIG_V5: &str = r#"
+[general]
+pid_file = "/tmp/hebo-tests/mqtt-1892.pid"
+
+[[listeners]]
+protocol = "mqtt"
+address = "0.0.0.0:1892"
+
+[security]
+allow_anonymous = false
+
+[log]
+log_file = "/tmp/hebo-tests/hebo-1892.log"
 "#;
 
 #[test]
-fn test_conn_allow_anonymous() -> Result<(), Error> {
+fn test_conn_allow_anonymous_v4() -> Result<(), Error> {
     let config = ServerConfig::new(
-        "/tmp/hebo-tests/01-connect-allow-anonymous.conf",
-        ALLOW_CONFIG,
+        "/tmp/hebo-tests/01-connect-allow-anonymous-v4.toml",
+        ALLOW_CONFIG_V4,
     )?;
     let mut server = Server::start(config.filename())?;
     // TODO(Shaohua): Run ruo client
@@ -52,10 +83,35 @@ fn test_conn_allow_anonymous() -> Result<(), Error> {
 }
 
 #[test]
-fn test_conn_deny_anonymous() -> Result<(), Error> {
+fn test_conn_allow_anonymous_v5() -> Result<(), Error> {
     let config = ServerConfig::new(
-        "/tmp/hebo-tests/01-connect-allow-anonymous.conf",
-        DENY_CONFIG,
+        "/tmp/hebo-tests/01-connect-allow-anonymous-v5.toml",
+        ALLOW_CONFIG_V5,
+    )?;
+    let mut server = Server::start(config.filename())?;
+    // TODO(Shaohua): Run ruo client
+    sleep(Duration::from_secs(5));
+    server.terminate();
+    Ok(())
+}
+
+#[test]
+fn test_conn_deny_anonymous_v4() -> Result<(), Error> {
+    let config = ServerConfig::new(
+        "/tmp/hebo-tests/01-connect-deny-anonymous-v4.toml",
+        DENY_CONFIG_V4,
+    )?;
+    let mut server = Server::start(config.filename())?;
+    sleep(Duration::from_secs(5));
+    server.terminate();
+    Ok(())
+}
+
+#[test]
+fn test_conn_deny_anonymous_v5() -> Result<(), Error> {
+    let config = ServerConfig::new(
+        "/tmp/hebo-tests/01-connect-deny-anonymous-v5.toml",
+        DENY_CONFIG_V5,
     )?;
     let mut server = Server::start(config.filename())?;
     sleep(Duration::from_secs(5));
