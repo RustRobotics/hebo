c31f5808a12aaff4c36e2a6dbf54af945fefc552
Xu Shaohua <shaohua@biofan.org>
2022-04-27 08:40:54 +0800

    codec: Simplify mod names

--- codec/src/v5/mod.rs
+++ codec/src/v5/mod.rs
@@ -2,48 +2,46 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
-mod auth_packet;
-mod connect_ack_packet;
-mod connect_packet;
-mod disconnect_packet;
+mod auth;
+mod connect;
+mod connect_ack;
+mod disconnect;
 mod header;
-mod ping_request_packet;
-mod ping_response_packet;
+mod ping_request;
+mod ping_response;
 mod property;
-mod publish_ack_packet;
-mod publish_complete_packet;
-mod publish_packet;
-mod publish_received_packet;
-mod publish_release_packet;
+mod publish;
+mod publish_ack;
+mod publish_complete;
+mod publish_received;
+mod publish_release;
 mod reason_code;
-mod subscribe_ack_packet;
-mod subscribe_packet;
-mod unsubscribe_ack_packet;
-mod unsubscribe_packet;
+mod subscribe;
+mod subscribe_ack;
+mod unsubscribe;
+mod unsubscribe_ack;
 
-pub use auth_packet::{AuthPacket, AUTH_PROPERTIES, AUTH_REASONS};
-pub use connect_ack_packet::{ConnectAckPacket, CONNECT_ACK_PROPERTIES, CONNECT_REASONS};
-pub use connect_packet::ConnectPacket;
-pub use disconnect_packet::{DisconnectPacket, DISCONNECT_PROPERTIES, DISCONNECT_REASONS};
+pub use auth::{AuthPacket, AUTH_PROPERTIES, AUTH_REASONS};
+pub use connect::ConnectPacket;
+pub use connect_ack::{ConnectAckPacket, CONNECT_ACK_PROPERTIES, CONNECT_REASONS};
+pub use disconnect::{DisconnectPacket, DISCONNECT_PROPERTIES, DISCONNECT_REASONS};
 pub use header::{FixedHeader, Packet, PacketType};
-pub use ping_request_packet::PingRequestPacket;
-pub use ping_response_packet::PingResponsePacket;
+pub use ping_request::PingRequestPacket;
+pub use ping_response::PingResponsePacket;
 pub use property::{Properties, Property, PropertyType};
-pub use publish_ack_packet::{PublishAckPacket, PUBLISH_ACK_PROPERTIES, PUBLISH_ACK_REASONS};
-pub use publish_complete_packet::{
+pub use publish::PublishPacket;
+pub use publish_ack::{PublishAckPacket, PUBLISH_ACK_PROPERTIES, PUBLISH_ACK_REASONS};
+pub use publish_complete::{
     PublishCompletePacket, PUBLISH_COMPLETE_PROPERTIES, PUBLISH_COMPLETE_REASONS,
 };
-pub use publish_packet::PublishPacket;
-pub use publish_received_packet::{
+pub use publish_received::{
     PublishReceivedPacket, PUBLISH_RECEIVED_PROPERTIES, PUBLISH_RECEIVED_REASONS,
 };
-pub use publish_release_packet::{
+pub use publish_release::{
     PublishReleasePacket, PUBLISH_RELEASE_PROPERTIES, PUBLISH_RELEASE_REASONS,
 };
 pub use reason_code::ReasonCode;
-pub use subscribe_ack_packet::{SubscribeAckPacket, SUBSCRIBE_ACK_PROPERTIES, SUBSCRIBE_REASONS};
-pub use subscribe_packet::SubscribePacket;
-pub use unsubscribe_ack_packet::{
-    UnsubscribeAckPacket, UNSUBSCRIBE_ACK_PROPERTIES, UNSUBSCRIBE_REASONS,
-};
-pub use unsubscribe_packet::{UnsubscribePacket, UNSUBSCRIBE_PROPERTIES};
+pub use subscribe::SubscribePacket;
+pub use subscribe_ack::{SubscribeAckPacket, SUBSCRIBE_ACK_PROPERTIES, SUBSCRIBE_REASONS};
+pub use unsubscribe::{UnsubscribePacket, UNSUBSCRIBE_PROPERTIES};
+pub use unsubscribe_ack::{UnsubscribeAckPacket, UNSUBSCRIBE_ACK_PROPERTIES, UNSUBSCRIBE_REASONS};
