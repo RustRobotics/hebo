6daeb27d517f2187b27f6655c1e1543e573d20cf
Xu Shaohua <shaohua@biofan.org>
2022-04-27 08:57:09 +0800

    ruo: Add client inner mod

--- ruo/src/blocking/mod.rs
+++ ruo/src/blocking/mod.rs
@@ -2,5 +2,8 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
+//! Blocking client is used for testing only.
+
 pub mod client;
+mod client_inner;
 pub mod stream;
