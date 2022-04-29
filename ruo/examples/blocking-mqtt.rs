d98b1f291d75e580eecbf94fda86ea2363521524
Xu Shaohua <shaohua@biofan.org>
2022-04-29 08:39:55 +0800

    ruo: Fix client initialization error in example

--- ruo/examples/blocking-mqtt.rs
+++ ruo/examples/blocking-mqtt.rs
@@ -2,10 +2,9 @@
 // Use of this source is governed by Affero General Public License that can be found
 // in the LICENSE file.
 
-use codec::{ProtocolLevel, QoS};
+use codec::QoS;
 use ruo::blocking::client::Client;
-use ruo::connect_options::{ConnectOptions, ConnectType, MqttConnect};
-use std::net::SocketAddr;
+use ruo::connect_options::ConnectOptions;
 
 use ruo::error::Error;
 
@@ -13,11 +12,7 @@ fn main() -> Result<(), Error> {
     std::env::set_var("RUST_LOG", "info");
     env_logger::init();
 
-    let mut options = ConnectOptions::new();
-    options.set_connect_type(ConnectType::Mqtt(MqttConnect {
-        address: SocketAddr::from(([127, 0, 0, 1], 1883)),
-    }));
-    let mut client = Client::new(options, ProtocolLevel::V31);
+    let mut client = Client::new(ConnectOptions::default());
     let is_connected = client.connect()?;
     log::info!("is connected");
     assert!(is_connected);
