a131e26eb340775b7362dfae0e31a9bea5b654e7
Xu Shaohua <shaohua@biofan.org>
2022-05-09 09:29:20 +0800

    ruo: Support unix domain socket in blocking client

---
+++ ruo/examples/blocking-uds.rs
@@ -0,0 +1,42 @@
+// Copyright (c) 2020 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Affero General Public License that can be found
+// in the LICENSE file.
+
+use codec::QoS;
+use ruo::blocking::client::Client;
+use ruo::connect_options::ConnectOptions;
+
+use ruo::error::Error;
+
+fn main() -> Result<(), Error> {
+    std::env::set_var("RUST_LOG", "info");
+    env_logger::init();
+
+    let mut options = ConnectOptions::new();
+    options.set_connect_type(ConnectType::Uds(UdsConnect {
+        sock_path: PathBuf::from("/tmp/hebo-uds.sock"),
+    }));
+    let mut client = Client::new(options);
+    client.connect()?;
+    log::info!(
+        "Connected to server, client id: {}",
+        client.connect_options().client_id()
+    );
+
+    client.subscribe("hello", QoS::AtMostOnce)?;
+    client.publish("hello", QoS::AtMostOnce, b"Hello, world")?;
+    loop {
+        if let Some(message) = client.wait_for_message()? {
+            log::info!("got message: {:?}", message);
+            break;
+        } else {
+            log::info!("No message");
+        }
+    }
+
+    // If client.disconnect() is not called explicitly, it will be called in
+    // drop().
+    //client.disconnect()?;
+
+    Ok(())
+}
--- ruo/src/blocking/stream.rs
+++ ruo/src/blocking/stream.rs
@@ -3,7 +3,7 @@
 // in the LICENSE file.
 
 use std::fmt;
-use std::io::{self, Read, Write};
+use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::os::unix::net::UnixStream;
 
@@ -12,12 +12,14 @@ use crate::error::{Error, ErrorKind};
 
 pub enum Stream {
     Mqtt(TcpStream),
+    Uds(UnixStream),
 }
 
 impl fmt::Debug for Stream {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::Mqtt(..) => f.write_str("Mqtt"),
+            Self::Uds(..) => f.write_str("Uds"),
         }
     }
 }
@@ -27,6 +29,7 @@ impl Drop for Stream {
         log::info!("Stream::drop()");
         match self {
             Stream::Mqtt(stream) => drop(stream),
+            Stream::Uds(stream) => drop(stream),
         }
     }
 }
@@ -35,21 +38,27 @@ impl Stream {
     pub fn new(connect_type: &ConnectType) -> Result<Self, Error> {
         match connect_type {
             ConnectType::Mqtt(mqtt_connect) => Stream::new_mqtt(mqtt_connect),
+            ConnectType::Uds(uds_connect) => Stream::new_uds(uds_connect),
             _ => todo!(),
         }
     }
 
     fn new_mqtt(mqtt_connect: &MqttConnect) -> Result<Self, Error> {
-        let socket = TcpStream::connect(mqtt_connect.address)?;
-        Ok(Stream::Mqtt(socket))
+        let stream = TcpStream::connect(mqtt_connect.address)?;
+        Ok(Stream::Mqtt(stream))
+    }
+
+    fn new_uds(uds_connect: &UdsConnect) -> Result<Stream, Error> {
+        let uds_stream = UnixStream::connect(&uds_connect.sock_path)?;
+        Ok(Stream::Uds(uds_stream))
     }
 
     pub fn read_buf(&mut self, buf: &mut [u8]) -> Result<usize, Error> {
         match self {
-            Stream::Mqtt(socket) => {
+            Stream::Mqtt(stream) => {
                 // let reference = std::io::Read::by_ref(socket);
                 // reference.take(buf.capacity() as u64).read_to_end(buf)
-                socket.read(buf).map_err(|err| {
+                stream.read(buf).map_err(|err| {
                     Error::from_string(
                         ErrorKind::SocketError,
                         format!(
@@ -60,16 +69,38 @@ impl Stream {
                     )
                 })
             }
+
+            Stream::Uds(stream) => stream.read(buf).map_err(|err| {
+                Error::from_string(
+                    ErrorKind::SocketError,
+                    format!(
+                        "Failed to read from uds stream, buffer len: {}, err: {:?}",
+                        buf.len(),
+                        err
+                    ),
+                )
+            }),
         }
     }
 
     pub fn write_all(&mut self, buf: &[u8]) -> Result<(), Error> {
         match self {
-            Stream::Mqtt(socket) => socket.write_all(buf).map_err(|err| {
+            Stream::Mqtt(stream) => stream.write_all(buf).map_err(|err| {
                 Error::from_string(
                     ErrorKind::SocketError,
                     format!(
                         "Failed to write {} bytes to mqtt stream, err: {:?}",
+                        buf.len(),
+                        err
+                    ),
+                )
+            }),
+
+            Stream::Uds(stream) => stream.write_all(buf).map_err(|err| {
+                Error::from_string(
+                    ErrorKind::SocketError,
+                    format!(
+                        "Failed to write {} bytes to uds stream, err: {:?}",
                         buf.len(),
                         err
                     ),
