da420bc0dae8aa5fa90ba6d9784156df0a711a3f
Xu Shaohua <shaohua@biofan.org>
2022-05-12 14:06:13 +0800

    codec: Fix lint errors in byte array

--- codec/src/byte_array.rs
+++ codec/src/byte_array.rs
@@ -6,6 +6,7 @@ use byteorder::{BigEndian, ByteOrder};
 
 use super::utils;
 
+#[allow(clippy::module_name_repetitions)]
 #[derive(Debug)]
 pub enum ByteArrayError {
     OutOfRangeError,
@@ -13,8 +14,8 @@ pub enum ByteArrayError {
 }
 
 impl From<utils::StringError> for ByteArrayError {
-    fn from(e: utils::StringError) -> ByteArrayError {
-        ByteArrayError::InvalidString(e)
+    fn from(e: utils::StringError) -> Self {
+        Self::InvalidString(e)
     }
 }
 
@@ -24,23 +25,40 @@ pub struct ByteArray<'a> {
 }
 
 impl<'a> ByteArray<'a> {
-    /// Create a new ByteArray object based on byte slice.
-    pub fn new(data: &'a [u8]) -> Self {
+    /// Create a new `ByteArray` object based on byte slice.
+    #[must_use]
+    pub const fn new(data: &'a [u8]) -> Self {
         ByteArray { offset: 0, data }
     }
 
     /// Get length of inner byte slice.
-    pub fn len(&self) -> usize {
+    #[must_use]
+    pub const fn len(&self) -> usize {
         self.data.len()
     }
 
+    /// Returns true if byte array is empty.
+    #[must_use]
+    pub const fn is_empty(&self) -> bool {
+        self.data.is_empty()
+    }
+
     /// Get remaining length of bytes available to read.
-    pub fn remaining_bytes(&self) -> usize {
+    ///
+    /// # Panics
+    ///
+    /// Runs into panic if lenght of inner byte slice is invalid.
+    #[must_use]
+    pub const fn remaining_bytes(&self) -> usize {
         assert!(self.offset <= self.data.len());
         self.data.len() - self.offset
     }
 
     /// Read one byte from slice.
+    ///
+    /// # Errors
+    ///
+    /// Returns error if the array has no length bytes.
     pub fn read_byte(&mut self) -> Result<u8, ByteArrayError> {
         self.offset += 1;
         if self.offset > self.data.len() {
@@ -51,22 +69,37 @@ impl<'a> ByteArray<'a> {
     }
 
     /// Read a u16 value from slice.
+    ///
+    /// # Errors
+    ///
+    /// Returns error if the array has no length bytes.
     pub fn read_u16(&mut self) -> Result<u16, ByteArrayError> {
         Ok(BigEndian::read_u16(self.read_bytes(2)?))
     }
 
     /// Read a u32 value from slice.
+    ///
+    /// # Errors
+    ///
+    /// Returns error if the array has no length bytes.
     pub fn read_u32(&mut self) -> Result<u32, ByteArrayError> {
         Ok(BigEndian::read_u32(self.read_bytes(4)?))
     }
 
     /// Read an UTF-8 string with `len` from slice.
+    ///
+    /// # Errors
+    ///
+    /// Returns error if the array has no length bytes or bytes are not valid utf8 string.
     pub fn read_string(&mut self, len: usize) -> Result<String, ByteArrayError> {
         let bytes = self.read_bytes(len)?;
         utils::to_utf8_string(bytes).map_err(ByteArrayError::from)
     }
 
     /// Read a byte array with `len` from slice.
+    /// # Errors
+    ///
+    /// Returns error if the array has no length bytes.
     pub fn read_bytes(&mut self, len: usize) -> Result<&[u8], ByteArrayError> {
         self.offset += len;
         if self.offset > self.data.len() {
@@ -82,7 +115,8 @@ impl<'a> ByteArray<'a> {
     }
 
     /// Get current offset.
-    pub fn offset(&mut self) -> usize {
+    #[must_use]
+    pub const fn offset(&self) -> usize {
         self.offset
     }
 }
