ae2faae15fa0783b5577cbd9bd59b8b0362f35be
Xu Shaohua <shaohua@biofan.org>
2022-05-18 08:04:50 +0800

    lint: Enable auto fix

--- tools/lint.sh
+++ tools/lint.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+# Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+# Use of this source is governed by Apache-2.0 License that can be found
+# in the LICENSE file.
 
 set -xe
 
-cargo clippy --all-targets --all-features -- \
+cargo clippy --all-targets --all-features --fix --allow-dirty --allow-staged -- \
   --deny warnings \
   --deny clippy::all \
   --deny clippy::cargo \
   --deny clippy::nursery \
   --deny clippy::pedantic
-cargo fmt --check
+
+cargo fmt --all
 
