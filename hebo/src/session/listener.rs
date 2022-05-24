675bb9a2ddad87626e187e1903b85f61a98693ec
Xu Shaohua <shaohua@biofan.org>
2022-05-24 10:53:12 +0800

    hebo: Handle v5 protocol commands in session app

--- hebo/src/session/listener.rs
+++ hebo/src/session/listener.rs
@@ -4,7 +4,7 @@
 
 //! Handles commands from listener.
 
-use codec::{v3, PacketId, QoS};
+use codec::{v3, v5, PacketId, QoS};
 
 use super::{Session, Status};
 use crate::commands::ListenerToSessionCmd;
@@ -20,29 +20,27 @@ impl Session {
             ListenerToSessionCmd::ConnectAck(packet, cached_session) => {
                 self.on_listener_connect_ack(packet, cached_session).await
             }
-            ListenerToSessionCmd::ConnectAckV5(_packet, _cached_session) => {
-                todo!()
+            ListenerToSessionCmd::ConnectAckV5(packet, cached_session) => {
+                self.on_listener_connect_ack_v5(packet, cached_session)
+                    .await
             }
             ListenerToSessionCmd::PublishAck(packet_id, qos, accepted) => {
                 self.on_listener_publish_ack(packet_id, qos, accepted).await
             }
-            ListenerToSessionCmd::PublishAckV5(_packet_id, _qos, _accepted) => {
-                todo!()
+            ListenerToSessionCmd::PublishAckV5(packet_id, qos, accepted) => {
+                self.on_listener_publish_ack_v5(packet_id, qos, accepted)
+                    .await
             }
             ListenerToSessionCmd::Publish(packet) => self.on_listener_publish(packet).await,
-            ListenerToSessionCmd::PublishV5(_packet) => {
-                todo!()
-            }
+            ListenerToSessionCmd::PublishV5(packet) => self.on_listener_publish_v5(packet).await,
             ListenerToSessionCmd::SubscribeAck(packet) => {
                 self.on_listener_subscribe_ack(packet).await
             }
-            ListenerToSessionCmd::SubscribeAckV5(_packet) => {
-                todo!()
+            ListenerToSessionCmd::SubscribeAckV5(packet) => {
+                self.on_listener_subscribe_ack_v5(packet).await
             }
             ListenerToSessionCmd::Disconnect => self.on_listener_disconnect().await,
-            ListenerToSessionCmd::DisconnectV5 => {
-                todo!()
-            }
+            ListenerToSessionCmd::DisconnectV5 => self.on_listener_disconnect().await,
         }
     }
 
@@ -57,6 +55,27 @@ impl Session {
 
         self.status = match return_code {
             v3::ConnectReturnCode::Accepted => Status::Connected,
+            _ => Status::Disconnected,
+        };
+
+        if let Some(cached_session) = cached_session {
+            self.load_cached_session(cached_session);
+        }
+
+        Ok(())
+    }
+
+    async fn on_listener_connect_ack_v5(
+        &mut self,
+        packet: v5::ConnectAckPacket,
+        cached_session: Option<CachedSession>,
+    ) -> Result<(), Error> {
+        // Send connect ack first, then update status.
+        let reason_code = packet.reason_code();
+        self.send(packet).await?;
+
+        self.status = match reason_code {
+            v5::ReasonCode::Success => Status::Connected,
             _ => Status::Disconnected,
         };
 
@@ -101,7 +120,47 @@ impl Session {
         }
         Ok(())
     }
+
+    /// Send ack to client.
+    async fn on_listener_publish_ack_v5(
+        &mut self,
+        packet_id: PacketId,
+        qos: QoS,
+        accepted: bool,
+    ) -> Result<(), Error> {
+        // If a Server implementation does not authorize a PUBLISH to be performed by a Client;
+        // it has no way of informing that Client. It MUST either make a positive acknowledgement,
+        // according to the normal QoS rules, or close the Network Connection [MQTT-3.3.5-2].
+        if !accepted {
+            return self.send_disconnect().await;
+        }
+
+        // Check qos and send publish ack packet to client.
+        if qos == QoS::AtLeastOnce {
+            let ack_packet = v5::PublishAckPacket::new(packet_id);
+            // TODO(Shaohua): Catch errors
+            self.send(ack_packet).await?;
+        } else if qos == QoS::ExactOnce {
+            // Check inflight messages overflow.
+            if self.pub_recv_packets.len() > self.config.max_inflight_messages() {
+                log::error!("session: Too many unacknowledged qos=2 messages, disconnect client!");
+                return self.send_disconnect().await;
+            }
+
+            // Send PublishReceived.
+            self.pub_recv_packets.insert(packet_id);
+            let ack_packet = v5::PublishReceivedPacket::new(packet_id);
+            // TODO(Shaohua): Catch errors
+            self.send(ack_packet).await?;
+        }
+        Ok(())
+    }
+
     async fn on_listener_publish(&mut self, packet: v3::PublishPacket) -> Result<(), Error> {
+        self.send(packet).await
+    }
+
+    async fn on_listener_publish_v5(&mut self, packet: v5::PublishPacket) -> Result<(), Error> {
         self.send(packet).await
     }
 
@@ -112,6 +171,14 @@ impl Session {
         // When the Server receives a SUBSCRIBE Packet from a Client, the Server MUST respond with a
         // SUBACK Packet [MQTT-3.8.4-1]. The SUBACK Packet MUST have the same Packet Identifier as the
         // SUBSCRIBE Packet that it is acknowledging [MQTT-3.8.4-2].
+        self.send(packet).await
+    }
+
+    async fn on_listener_subscribe_ack_v5(
+        &mut self,
+        packet: v5::SubscribeAckPacket,
+    ) -> Result<(), Error> {
+        // TODO(Shaohua): Add comments
         self.send(packet).await
     }
 
