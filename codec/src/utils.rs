263c1a30ce2e73ac39b9167df5895db3082a856d
Xu Shaohua <shaohua@biofan.org>
2022-05-26 10:49:55 +0800

    codec: Allow extra characters in client id

--- codec/src/utils.rs
+++ codec/src/utils.rs
@@ -5,6 +5,8 @@
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
 
+pub const MAXIMUM_CLIENT_ID: usize = 23;
+
 /// Generate random string.
 #[must_use]
 pub fn random_string(len: usize) -> String {
@@ -124,22 +126,30 @@ pub fn to_utf8_string(buf: &[u8]) -> Result<String, StringError> {
 /// `ClientId` is based on rules below:
 ///
 /// - The `ClientId` MUST be a UTF-8 encoded string as defined in Section 1.5.3 [MQTT-3.1.3-4].
-///
 /// - The Server MUST allow `ClientIds` which are between 1 and 23 UTF-8 encoded bytes in length, and that
 ///   contain only the characters
 ///   "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" [MQTT-3.1.3-5].
+/// - And '-_.' characters are allowed
 ///
 /// # Errors
 ///
 /// Returns error if `id` is too large or contains invalid chars.
 pub fn validate_client_id(id: &str) -> Result<(), StringError> {
-    if id.is_empty() || id.len() > 23 {
+    if id.is_empty() {
+        log::error!("client id is empty");
+        return Err(StringError::InvalidLength);
+    }
+    if id.len() > MAXIMUM_CLIENT_ID {
+        log::error!("client id has too many charas: {}", id.len());
         return Err(StringError::InvalidLength);
     }
     for byte in id.bytes() {
         if !((b'0'..=b'9').contains(&byte)
             || (b'a'..=b'z').contains(&byte)
-            || (b'A'..=b'Z').contains(&byte))
+            || (b'A'..=b'Z').contains(&byte)
+            || b'-' == byte
+            || b'_' == byte
+            || b'.' == byte)
         {
             return Err(StringError::InvalidChar);
         }
